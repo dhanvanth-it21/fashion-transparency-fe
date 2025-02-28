47f09ef7e587640fd2a6247efeb4b5064516c6c4
dhanvanth-it21 <dhanvanth.it21@bitsathy.ac.in>
2025-02-28 08:25:36 +0530

    inventory tiles list on-work

--- src/app/features/admin/inventory/inventory.component.ts
+++ src/app/features/admin/inventory/inventory.component.ts
@@ -2,23 +2,37 @@ import { Component } from '@angular/core';
 import { ActivatedRoute, NavigationEnd, Router, RouterModule } from '@angular/router';
 import { AddTileComponent } from "./add-tile/add-tile.component";
 import { CommonModule } from '@angular/common';
+import { ApiService } from '../../../shared/services/api.service';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { Tile } from '../models/tile.modle';
+import { MatIconModule } from '@angular/material/icon'
 
 @Component({
   selector: 'app-inventory',
   standalone: true,
-  imports: [RouterModule, AddTileComponent, CommonModule],
+  imports: [RouterModule, AddTileComponent, CommonModule, MatTableModule, MatIconModule],
   templateUrl: './inventory.component.html',
   styleUrl: './inventory.component.css'
 })
 export class InventoryComponent {
   isAddTileComponentOpen: Boolean = false;
-
+  
+  public displayedColumns = ['skuCode', 'brandName', 'modelName', 'tileSize', 'quantity/pieces', 'details', 'update', 'delete'
+  ];
+  public dataSource = new MatTableDataSource<Tile>();
+  
   constructor(
     private router: Router,
     private activatedRoute: ActivatedRoute,
+    private apiService: ApiService,
   ) {}
-
+  
   ngOnInit() {
+
+
+    this.getTilesList();
+
+
     if(this.router.url === "/admin/inventory/add-tile") {
       this.isAddTileComponentOpen = true;
     }
@@ -41,6 +55,30 @@ export class InventoryComponent {
   
   closeAddTileComponent() {
     this.router.navigate(["/admin/inventory"]);
+  }
+
+  getTilesList() {
+    this.apiService.getTilesList().subscribe(
+      {
+        next : (response: any) => {
+          if (response.status === "success" && response.data) {
+            this.dataSource.data = response.data as Tile[];
+            console.log(response.data);
+        } 
+        },
+        error : (e) => {console.error(e)},
+      }
+    )
+  }
+
+  public redirectToDetails = (id: string) => {
+    
+  }
+  public redirectToUpdate = (id: string) => {
+    
+  }
+  public redirectToDelete = (id: string) => {
+    
   }
 
 }
---
+++ src/app/features/admin/models/tile.modle.ts
@@ -0,0 +1,47 @@
+export enum TileSize {
+    SIZE_1X1 = "1x1",
+    SIZE_1_5X1 = "1.5x1",
+    SIZE_2X1 = "2x1",
+    SIZE_2X2 = "2x2",
+    SIZE_3X3 = "3x3",
+    SIZE_4X2 = "4x2",
+    SIZE_4X4 = "4x4",
+    SIZE_6X4 = "6x4",
+    SIZE_8X4 = "8x4"
+}
+
+export enum TileCategory {
+    WALL = "WALL",
+    FLOOR = "FLOOR"
+}
+
+export enum Finishing {
+    GLOSSY = "GLOSSY",
+    HIGH_GLOSSY = "HIGH_GLOSSY",
+    MATT = "MATT",
+    SUGAR = "SUGAR",
+    CARVING = "CARVING",
+    FULL_BODY = "FULL_BODY"
+}
+
+export enum TileSubCategory {
+    // For wall
+    KITCHEN = "KITCHEN",
+    BATHROOM = "BATHROOM",
+    ELEVATION = "ELEVATION",
+
+    // For floor
+    INDOOR = "INDOOR",
+    PARKING = "PARKING",
+    ROOFING = "ROOFING"
+}
+
+export interface Tile {
+    _id: string;
+    skuCode: string;
+    tileSize: TileSize;
+    brandName: string;
+    modelName: string;
+    qty: number;
+    piecesPerBox: number;
+}
